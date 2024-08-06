2b9082047427d55680612ff962670f0d18ce559c
Aditya Tawade <62465343+adityatawade12@users.noreply.github.com>
2024-08-06 05:52:43 +0000

    model 7

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -4,7 +4,26 @@ import mat
 
 def reward_function(params):  
 
+    #distnce from centre
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.7
+    elif distance_from_center <= marker_3:
+        reward = 0.4
+    else:
+        reward = 1e-3 
 
+    #reward in angle
     # Read input variables
 
     waypoints = params['waypoints']
@@ -17,7 +36,7 @@ def reward_function(params):
 
     # Initialize the reward with typical value
 
-    reward = 1.0
+    # reward = 1.0
 
     # Calculate the direction of the center line based 
 
@@ -45,8 +64,19 @@ def reward_function(params):
 
     DIRECTION_THRESHOLD = 10.0
 
-    if direction_diff > DIRECTION_THRESHOLD:
+    if direction_diff > 20:
+        reward *= 0.1
+    elif direction_diff > 10:
+        reward *=0.5
+    elif direction_diff > 5:
+        reward *=0.8
+    elif direction_diff == 0:
+        reward *=1.2
 
-        reward *= 0.5
+    
+   
+
+    
 
     return float(reward)
+
